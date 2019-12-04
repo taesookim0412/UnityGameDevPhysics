6817cc0b48efe04aed8fc805cbd65e42bd86b754
Tae Soo Kim <taesoo.kim0412@gmail.com>
2019-12-04 06:54:00 -0800

    RealmRush Intro w/ Couroutines(EnemyMovement)

---
+++ RealmRush/EnemyMovement.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour
+{
+    [SerializeField] List<Waypoint> path;
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(PrintAllWaypoints());
+    }
+
+    IEnumerator PrintAllWaypoints()
+    {
+        foreach (Waypoint waypoint in path)
+        {
+            transform.position = waypoint.transform.position;
+            print("Visiting: " + waypoint.name);
+            yield return new WaitForSeconds(1f);
+        }
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
